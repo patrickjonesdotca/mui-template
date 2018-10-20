fe1b2a6b87ba21c0bfc98b586e93642e652239fb
Patrick Jones <patrick@dealmaker.tech>
2018-10-20 00:27:27 -0400

    devising...

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root to: 'home#index'
   resources :home, only: [:index]
   resources :about, only: [:index]
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
